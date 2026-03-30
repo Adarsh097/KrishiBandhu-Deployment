884d51a492dc91dc82f655527e78d2695bde5f60
Adarsh Gupta <adarshgupta0601@gmail.com>
2026-03-31 01:14:49 +0530

    changes

--- terraform/jenkins/install_jenkins.sh
+++ terraform/jenkins/install_jenkins.sh
@@ -34,3 +34,9 @@ sudo usermod -aG docker jenkins
 sudo systemctl restart docker
 sudo systemctl restart jenkins
 
+# Install dependencies and Trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release snapd -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update -y
+sudo apt-get install trivy -y
